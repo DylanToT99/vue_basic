c53426e5640c17c07282c639795435f6a73d44d3
Dylan <1975131479@qq.com>
2022-10-12 20:37:16 +0800

    vue_basic

---
+++ 19_单文件组件/School.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+      <div>
+            <h2>学校名称:{{ schoolName }}</h2>
+            <h2>学校地址:{{ address }}</h2>
+         <button @click="showName">点我提示学校名</button>
+     </div>
+</template>
+<script>
+export default {
+    name:'School',
+    data() {
+        return {
+            schoolName:'武汉大学',
+            address:'北京昌平'
+        }
+    },
+    methods:{
+        showName(){
+            alert(this.schoolName)
+        }
+    }
+}
+</script>
+<style>
+
+</style>
---
+++ 19_单文件组件/Student.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+      <div>
+            <h2>学生名称:{{ studentName }}</h2>
+            <h2>学校年龄:{{ age }}</h2>
+         <button @click="showName">点我提示学生名</button>
+     </div>
+</template>
+<script>
+export default {
+    name:'Student',
+    data() {
+        return {
+            schoolName:'武汉大学',
+            age: 18
+        }
+    },
+    methods:{
+        showName(){
+            alert(this.studentName)
+        }
+    }
+}
+</script>
+<style>
+
+</style>
